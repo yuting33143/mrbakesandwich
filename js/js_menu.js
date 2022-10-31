72d2d09f74b9908e7cb3c5e3af83d6073017e4f1
yuting33143 <cora33143@gmail.com>
2022-10-31 09:36:05 +0800

    add menu scroll

--- js/js_menu.js
+++ js/js_menu.js
@@ -1,9 +1,10 @@
+//向上滑按鈕 滾動時出現
 document.addEventListener('scroll',up_scroll);
 
 
 
 
-
+//向上滑按鈕 滾動時出現
 function up_scroll(){
     let up = document.getElementById('up');
     let scroll_height = window.pageYOffset;
