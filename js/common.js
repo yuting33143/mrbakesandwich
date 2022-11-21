c655b5df274764247fe40e03b0b9080e336d1bfd
yuting33143 <cora33143@gmail.com>
2022-11-21 19:06:06 +0800

    header

--- js/common.js
+++ js/common.js
@@ -1,7 +1,6 @@
 // 滾動header固定換色
-document.addEventListener('scroll',headerscroll);
 
-// 滾動header固定換色
+document.addEventListener('scroll',headerscroll);
 function headerscroll(){
     let headerscroll = document.getElementById("header");
     let a_scroll = document.querySelectorAll('.top ul > li > a');
@@ -24,6 +23,33 @@ function headerscroll(){
         ham_scroll.classList.remove('ham_scroll');
     }
 }
+//點擊漢堡線 ul跑出來
+$('#i_click').on('click',function(){
+    
+    if(scroll_height>100 ){
+        $('#ul_show').toggleClass('ham_ul_show');
+        $('header').toggleClass('ham_bg')
+    }
+})
+
+//  document.querySelector('header i').addEventListener('click',ham_click);
+/*
+function ham_click(){
+    let ul_show = document.getElementById('ul_show');
+    let ham_bg = document.querySelector('header');
+    let scroll_height = window.pageYOffset;
+    let img_scroll = document.getElementById('img_scroll');
+    let ham_scroll = document.querySelector('header i');
+    ul_show.classList.toggle('ham_ul_show');
+    ham_bg.classList.toggle('ham_bg');
+    if(scroll_height>100 ){
+        img_scroll.setAttribute('src','img/logo_w.png');
+        ham_scroll.classList.remove('ham_scroll');
+    }
+    
+}
+*/
+
 
 // 當手機版時 header 關於我們 a失效
 
@@ -47,16 +73,6 @@ $('#ol_show').on('mouseleave',function(){
     $(this).find('li').css('display','none');
 })
 
-
-//點擊漢堡線 ul跑出來
-document.querySelector('header i').addEventListener('click',ham_click);
-
-function ham_click(){
-    let ul_show = document.getElementById('ul_show');
-    let ham_bg = document.querySelector('header');
-    ul_show.classList.toggle('ham_ul_show');
-    ham_bg.classList.toggle('ham_bg');
-}
 
 
 
